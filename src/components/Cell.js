7896790de5058a3ab99f717ea4f3d5604078c82c
ACVBdeveloper <124077498+ACVBdeveloper@users.noreply.github.com>
2023-03-02 21:57:07 -0300

    commit inicial do tetris

---
+++ src/components/Cell.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {StyledCell} from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
+const Cell = ({ type }) => (
+    <StyledCell>Cell</StyledCell>
+);
+
+export default Cell;
